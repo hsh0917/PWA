f2344f6e10749d39e1543a1b81c502ce027c45cb
Lee <seolee@ohri.ca>
2019-05-06 09:35:45 -0400

    Updates

---
+++ precache-manifest.b0d247ace8a69356458d0d15f7b7697d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3ccfa6cee4c2e25804e2d4b4e37db7",
+    "url": "http://localhost:5000/index.html"
+  },
+  {
+    "revision": "e87ab869f88dbb9759fc",
+    "url": "http://localhost:5000/static/css/2.b4c5b390.chunk.css"
+  },
+  {
+    "revision": "d74a3aa7bb2c34fc96cc",
+    "url": "http://localhost:5000/static/css/main.5baaca57.chunk.css"
+  },
+  {
+    "revision": "e87ab869f88dbb9759fc",
+    "url": "http://localhost:5000/static/js/2.5b401000.chunk.js"
+  },
+  {
+    "revision": "d74a3aa7bb2c34fc96cc",
+    "url": "http://localhost:5000/static/js/main.7a4e3c1a.chunk.js"
+  },
+  {
+    "revision": "32562c57395eea87bbb8",
+    "url": "http://localhost:5000/static/js/runtime~main.e43d5d75.js"
+  },
+  {
+    "revision": "05329b7c697859259c01eef66e2ddee8",
+    "url": "http://localhost:5000/static/media/landing_image.05329b7c.jpg"
+  }
+]);
