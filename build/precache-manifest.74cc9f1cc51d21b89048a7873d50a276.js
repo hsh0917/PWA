14a884936c99ea097a7eeedf46cdd292a81a3f16
Lee <seolee@ohri.ca>
2019-05-09 11:08:25 -0400

    UI fixed

---
+++ build/precache-manifest.74cc9f1cc51d21b89048a7873d50a276.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3865006fa148dc616c7f4dfffb10024d",
+    "url": "https://toh-pwa-app.herokuapp.com/index.html"
+  },
+  {
+    "revision": "7ab4daf0d5ae9374f5b0",
+    "url": "https://toh-pwa-app.herokuapp.com/static/css/2.b4c5b390.chunk.css"
+  },
+  {
+    "revision": "9d39e72163e138e31663",
+    "url": "https://toh-pwa-app.herokuapp.com/static/css/main.4303289d.chunk.css"
+  },
+  {
+    "revision": "7ab4daf0d5ae9374f5b0",
+    "url": "https://toh-pwa-app.herokuapp.com/static/js/2.f102575c.chunk.js"
+  },
+  {
+    "revision": "9d39e72163e138e31663",
+    "url": "https://toh-pwa-app.herokuapp.com/static/js/main.fba327c4.chunk.js"
+  },
+  {
+    "revision": "cfe1eaa4b4561d1f458c",
+    "url": "https://toh-pwa-app.herokuapp.com/static/js/runtime~main.27f769be.js"
+  },
+  {
+    "revision": "05329b7c697859259c01eef66e2ddee8",
+    "url": "https://toh-pwa-app.herokuapp.com/static/media/landing_image.05329b7c.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("http://localhost:5000/precache-manifest.c4077c4e9f864e2bd1c5f9a7ca9b57e6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://toh-pwa-app.herokuapp.com/precache-manifest.74cc9f1cc51d21b89048a7873d50a276.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 self.addEventListener('install', function(event) {
   // The promise that skipWaiting() returns can be safely ignored.
