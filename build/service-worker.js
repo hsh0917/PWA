82854a2196d9f3050ab1f158f654b90562b5f737
Lee <seolee@ohri.ca>
2019-05-09 10:59:53 -0400

    UI fixed

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://toh-pwa-app.herokuapp.com/#/precache-manifest.5970e3f717cb39efdb592e02ea11fabe.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("http://localhost:5000/precache-manifest.c4077c4e9f864e2bd1c5f9a7ca9b57e6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 self.addEventListener('install', function(event) {
   // The promise that skipWaiting() returns can be safely ignored.
